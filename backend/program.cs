163dcaa9a3817b5a09223cfd38483dada2e458ff
Artur <only.up.ak@gmail.com>
2026-01-17 22:45:47 +0300

    Input name

--- backend/program.cs
+++ backend/program.cs
@@ -1,4 +1,16 @@
-﻿Console.WriteLine("Hello");
-Console.WriteLine("Kuzvah");
-Console.WriteLine(DateTime.Now);
-Console.WriteLine(DateTime.Now);
+﻿
+using System;
+using System.Globalization;
+using System.Text;
+
+Console.OutputEncoding = Encoding.UTF8;
+
+Console.WriteLine("Hello, World!");
+string a = "y";
+
+while (a == "y")
+{
+    Console.Write("Введи имя: ");
+    string name = Console.ReadLine();
+
+}
